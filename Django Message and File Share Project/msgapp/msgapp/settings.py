cad6b9fcb1e016d09fc7d0e23d52ccd73c52f20c
Kapil-Guru <64691323+Kapil-Guru@users.noreply.github.com>
2022-12-16 20:57:27 +0530

    Update settings.py

--- Django Message and File Share Project/msgapp/msgapp/settings.py
+++ Django Message and File Share Project/msgapp/msgapp/settings.py
@@ -20,9 +20,6 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'XXXX'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
